ee8c5eba52c969db685ae2f52a27c0e9b1cf78f1
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2024-09-09 14:44:41 +0200

    Hassle to fix that, nevermind

--- Week2/MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ Week2/MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b881dfc5f2d23cfff009d6f6acab328ba055650e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b9bbd2e91a39c6c730fd19cb3ab20b296ff4b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
